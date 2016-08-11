81ca0236886857314c99efb3e93a28cb590a3721
Piotr Król <piotr.krol@3mdeb.com>
2016-08-11 14:01:53 +0200

    add support for limiting rev-list to given branch

--- get-min-diff.sh
+++ get-min-diff.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-revs=$(git rev-list --all)
+if [ "$#" -eq 1  ]; then
+  revs=$(git rev-list $1)
+else
+  revs=$(git rev-list --all)
+fi
 
 for r in $revs; do
   df=$(git diff --stat $r|grep changed|cut -d" " -f2)
@@ -10,6 +14,7 @@ for r in $revs; do
     min_rev=$r
   fi
 
+  #echo "$min -gt $df"
   if [ $min -gt $df ]; then
     min=$df
     min_rev=$r
